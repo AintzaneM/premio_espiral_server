a7a9749c38d8acf338f3be57a8b651881a20a1bb
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-08 19:06:38 +0100

    set up morgan, cors & premioRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import config from "./config";
+import morgan from "morgan"
+import cors from "cors";
+import premioRoutes from "./routes/premios.routes"
 
 const app = express()
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", config.PORT);
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(premioRoutes)
 
 export default app

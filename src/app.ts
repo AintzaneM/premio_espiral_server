59339a1fa2ee945918389b7695222ebdb46e214a
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-08 21:34:37 +0100

    setup google auth, express session, passport

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,12 @@ import express from "express";
 import config from "./config";
 import morgan from "morgan"
 import cors from "cors";
-import premioRoutes from "./routes/premios.routes"
+import passport from "passport";
+const User = require("./routes/User");
+const { Strategy } = require('passport-google-oauth20')
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SESSION_SECRET } =  process.env
+import premioRoutes from "./routes/premios.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express()
 
@@ -12,7 +17,29 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(require('express-session')({ secret: SESSION_SECRET, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Strategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: '/return'
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    return cb(null, profile);
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
 
 app.use(premioRoutes)
+app.use(userRoutes)
 
 export default app
